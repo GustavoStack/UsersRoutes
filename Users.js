ae2fb895219267bdf27316c5a9eab33678cd853f
Gustavo Teixeira <104082561+GustavoStack@users.noreply.github.com>
2022-04-20 11:29:46 -0300

    Add files via upload

---
+++ Users.js
@@ -0,0 +1,14 @@
+const users = [
+    {
+        id:1,
+        name:'jorge',
+        email:'jorge@exemplo.com'
+    },
+    {
+        id:2,
+        name:'joão',
+        email:'joão@exemplo.com'
+    }
+]
+
+module.exports = users
